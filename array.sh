6342b220d4cac4a921ab9e300e92a2010fb729a5
satya <satyaprasad8660@gmail.com>
2025-07-23 18:22:30 +0530

    hellow world start

--- array.sh
+++ array.sh
@@ -6,4 +6,4 @@ Movies=("court" "hit3" "puspha2" "thandel" "kubhera")
 echo "First Movie: ${Movies[0]}"
 echo "First Movie: ${Movies[3]}"
 
-echo "All Movies: {Movies[@]}"
+echo "All Movies: ${Movies[@]}"
