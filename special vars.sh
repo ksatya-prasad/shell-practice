0ad2c724461a939a2fbeb33421d6310513bf5395
satya <satyaprasad8660@gmail.com>
2025-07-23 19:08:32 +0530

    hellow world start

--- special vars.sh
+++ special vars.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo "All variables passed to the script: $@"
-echo "number of variables: $#"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Current directory: $PWD"
+echo "User running this script: $USER"
+echo "Home directory of the user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of the last command running in background: $!"
